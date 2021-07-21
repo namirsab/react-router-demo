1d847d4cc3501727f99e21666f137f4893eb4d3a
Namir Sayed-Ahmad Baraza <namirsab@gmail.com>
2021-07-21 16:40:36 +0200

    Add Example app route hierarchy

--- src/App.js
+++ src/App.js
@@ -1,7 +1,44 @@
 import "./App.css";
+import { Switch, Route, NavLink } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <div className="App">
+      <header class="App__header">
+        <nav class="App__navigation">
+          <NavLink exact to="/">
+            Home
+          </NavLink>
+          <NavLink to="/about">About</NavLink>
+          <NavLink to="/contact">Contact</NavLink>
+        </nav>
+      </header>
+      <main>
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/contact">Contact</Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </main>
+      <footer>
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/contact">Contact</Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
